466d71ec809597ad7700c62ca28317a1e9b01aff
hakankoralturk <hakan@koralturk.com>
2023-07-08 02:47:30 +0300

    İpek's Xylophone

--- Xylophone/AppDelegate.swift
+++ Xylophone/AppDelegate.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-@main
+@UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
@@ -15,6 +15,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         return true
+    }
+
+    func applicationWillTerminate(_ application: UIApplication) {
+        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
 
     // MARK: UISceneSession Lifecycle
@@ -33,4 +37,5 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
 }
+
 
--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -6,14 +6,41 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
+    
+    var audioPlayer: AVAudioPlayer!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
+    
+    func playSound(sound: String) {
+        
+        let url = Bundle.main.url(forResource: sound, withExtension: "wav")
+        do {
+            
+            /* All time playing, silent or no silent */
+            try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default)
+            try AVAudioSession.sharedInstance().setActive(true)
+            
+            audioPlayer = try! AVAudioPlayer(contentsOf: url!)
+            audioPlayer.play()
+            
+        } catch let error {
+            print(error.localizedDescription)
+        }
+    }
+    
+    @IBAction func pressedKey(_ sender: UIButton) {
+        
+        playSound(sound: sender.currentTitle!)
+        sender.alpha = 0.5
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
+            sender.alpha = 1.0
+        }
+    }
 }
 
